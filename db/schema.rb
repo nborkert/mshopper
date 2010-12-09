e2c39ba29cd699b101c9aa84c25c508dc500b4a7
Neal Borkert <ndborkedaunt@neal-borkerts-imac-2.local>
2010-12-08 20:09:27 -0500

    creating stations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101209010522) do
+ActiveRecord::Schema.define(:version => 20101209010829) do
 
   create_table "clients", :force => true do |t|
     t.string   "contact_name"
@@ -49,6 +49,22 @@ ActiveRecord::Schema.define(:version => 20101209010522) do
     t.string   "phone_number"
     t.string   "referral_code"
     t.decimal  "referral_rate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stations", :force => true do |t|
+    t.string   "name"
+    t.decimal  "lat",               :precision => 10, :scale => 7
+    t.decimal  "lon",               :precision => 10, :scale => 7
+    t.string   "street_addr_line1"
+    t.string   "street_addr_line2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "description"
+    t.date     "enrollment_date"
+    t.integer  "client_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
