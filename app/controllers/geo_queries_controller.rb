75f195b87fb9b8809812b00743050a54431ce20a
Neal Borkert <ndborkedaunt@neal-borkerts-imac-2.local>
2011-01-06 21:37:59 -0500

    listing google map URL showing all driver queries since production deployment

--- app/controllers/geo_queries_controller.rb
+++ app/controllers/geo_queries_controller.rb
@@ -5,7 +5,13 @@ class GeoQueriesController < ApplicationController
   # GET /geo_queries.xml
   def index
     puts 'getting geo queries'
-    @geo_queries = GeoQuery.all
+    @geo_queries = GeoQuery.where('id > 238')  #prior queries were test results from me
+	#@geo_queries = GeoQuery.where('id > 10')  #prior queries were test results from me
+	
+	@final_url = 'http://maps.google.com/maps/api/staticmap?zoom=1&size=1024x1024&sensor=false&markers=size:tiny'
+	@geo_queries.each do |qry|
+	  @final_url = @final_url + '|' + qry.lat.to_s + ',' +qry.lon.to_s 	
+	end
 
     respond_to do |format|
       format.html # index.html.erb
