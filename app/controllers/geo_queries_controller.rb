1b29f178a6e1d2e5e3470d0ca8b8d764a008accb
Neal Borkert <ndborkedaunt@neal-borkerts-imac-2.local>
2011-01-06 21:53:46 -0500

    shortening URL correctly

--- app/controllers/geo_queries_controller.rb
+++ app/controllers/geo_queries_controller.rb
@@ -5,12 +5,12 @@ class GeoQueriesController < ApplicationController
   # GET /geo_queries.xml
   def index
     puts 'getting geo queries'
-    @geo_queries = GeoQuery.where('id > 238')  #prior queries were test results from me
-	#@geo_queries = GeoQuery.where('id > 10')  #prior queries were test results from me
+    #@geo_queries = GeoQuery.where('id > 238')  #prior queries were test results from me
+	@geo_queries = GeoQuery.where('id > 10')  #prior queries were test results from me
 	
 	@final_url = 'http://maps.google.com/maps/api/staticmap?zoom=1&size=1024x1024&sensor=false&markers=size:tiny'
 	@geo_queries.each do |qry|
-	  @final_url = @final_url + '|' + qry.lat.to_s.str[0,7] + ',' +qry.lon.to_s.str[0,7]	
+	  @final_url = @final_url + '|' + qry.lat.to_s.slice(0,7) + ',' +qry.lon.to_s.slice(0,7)	
 	end
 
     respond_to do |format|
